c609676dec5e77966afe776c24aa2481e52b278d
Shammi <ar98369@fhss.sjp.ac.lk>
2022-02-24 10:31:48 +0530

    Added doubly linked list

--- Elec/new.py
+++ Elec/new.py
@@ -4,3 +4,14 @@ class Node:
         self.item = data
         self.next = None
         self.prev = None
+# Class for doubly Linked List
+class doublyLinkedList:
+    def __init__(self):
+        self.start_node = None
+    # Insert Element to Empty list
+    def InsertToEmptyList(self, data):
+        if self.start_node is None:
+            new_node = Node(data)
+            self.start_node = new_node
+        else:
+            print("The list is empty")
